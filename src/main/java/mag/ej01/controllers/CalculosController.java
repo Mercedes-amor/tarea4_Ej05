0b0782996fab1b2a2822c94ac37a341461d8e754
MercedesAmorGallart <127596167+MercedesAmorGallart@users.noreply.github.com>
2024-11-20 11:37:08 +0100

    Ejercicio 5 de la tarea 4

--- src/main/java/mag/ej01/controllers/CalculosController.java
+++ src/main/java/mag/ej01/controllers/CalculosController.java
@@ -1,13 +1,10 @@
 package mag.ej01.controllers;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import mag.ej01.services.CalculosService;
 
@@ -17,79 +14,33 @@ public class CalculosController {
     @Autowired(required = true)
     private CalculosService calculosService;
 
+    private String txtStatus = null;
+
     @GetMapping("/")
-    public String showIndex() {
+    public String showInit(Model model) {
+        if (txtStatus != null) {
+            model.addAttribute("txtStatus", txtStatus);
+            txtStatus = null; // vacía la variable para poder usarla de nuevo
+        }
         return "indexView";
-
     }
 
-    @GetMapping("/calculos/primo/")
-    public String showPrimo(@RequestParam(required = false) Integer numero, Model model) {
-
-        // Clausula seguridad, número obligatorio y mayor que cero
-        if (numero == null || numero <= 0) {
-            model.addAttribute("error", "El parámetro 'numero' es obligatorio y debe ser mayor que cero.");
-            return "errorView";
-        }
-
-        try {
-            // Lógica principal en el @Service
-            boolean numeroEsPrimo = calculosService.esPrimo(numero);
-            model.addAttribute("numeroAComprobar", numero);
-            model.addAttribute("textoPrimo", numeroEsPrimo ? " es primo" : " no es primo");
-            return "indexView";
-        } catch (Exception e) {
-            model.addAttribute("error", "Ocurrió un error al procesar la solicitud.");
-            return "errorView";
-        }
-    }
-
-    @GetMapping("/calculos/hipotenusa/{x}/{y}")
-    public String showHipotenusa(@PathVariable double x,
-            @PathVariable(required = false) double y,
+    @GetMapping("/calcularHipotenusa/{cat1}/{cat2}")
+    public String showHipot(@PathVariable String cat1, @PathVariable String cat2,
             Model model) {
-
-        // Clausula seguridad, los catetos deben ser mayor que cero
-        if (x <= 0 || y <= 0) {
-            model.addAttribute("error", "Los catetos deben ser mayores que cero.");
-            return "errorView";
-        }
-
+        Double cateto1, cateto2;
         try {
-            // Cálculo de la hipotenusa
-            double hipotenusa = calculosService.calcularHipotenusa(x, y);
-            model.addAttribute("catetoX", x);
-            model.addAttribute("catetoY", y);
-            model.addAttribute("hipotenusa", hipotenusa);
-            return "hipotenusaView";
-        } catch (Exception e) {
-            model.addAttribute("error", "Ocurrió un error al calcular la hipotenusa.");
-            return "errorView";
+            cateto1 = Double.parseDouble(cat1); // puede lanzar excepción
+            cateto2 = Double.parseDouble(cat2);
+            model.addAttribute("resultado",
+                    calculosService.calcularHipotenusa(cateto1, cateto2)); // otra excepción
+            return "resultadoView";
+        } catch (NumberFormatException ex) { 
+            //Creamos mensaje personalizado para la excepción NumberFormatException (cuando no es un número)
+            txtStatus = "Algún cateto no numérico";
+        } catch (RuntimeException ex) {
+            txtStatus = ex.getMessage();
         }
+        return "redirect:/";
     }
-
-    @GetMapping("/calculos/divisores/{x}")
-    public String mostrarDivisores(@PathVariable int x, Model model) {
-
-        // Clausula seguridad, x debe ser mayor que cero
-        if (x <= 0) {
-            model.addAttribute("error", "el número para saber sus divisores debe ser mayor que cero");
-            return "errorView";
-        }
-
-        try {
-            // Creamos el array de Integer con los divisores
-            List<Integer> divisores = calculosService.calcularDivisores(x);
-
-            model.addAttribute("numero", x);
-            model.addAttribute("divisores", divisores);
-
-            return "divisoresView";
-        } catch (Exception e) {
-            model.addAttribute("error", "Ocurrió un error al calcular los divisores.");
-            return "errorView";
-        }
-
-    }
-
 }
--- src/main/java/mag/ej01/services/CalculosService.java
+++ src/main/java/mag/ej01/services/CalculosService.java
@@ -1,39 +1,15 @@
 package mag.ej01.services;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.stereotype.Service;
 
 @Service
 public class CalculosService {
-
-    public boolean esPrimo(Integer numero) {
-
-        for (int i = 2; i <= Math.sqrt(numero); i++) {
-            if (numero % i == 0) {
-                return false; // Si es divisible por algún número distinto de 1 y sí mismo, no es primo
-            }
+    public Double calcularHipotenusa (Double cat1, Double cat2) throws RuntimeException {
+ 
+        //Condicional excepción catetos menores que cero o mayores que 1000
+        if (cat1 <= 0 || cat2 <= 0)
+        throw new RuntimeException("Algún cateto menor que cero");
+        if (cat1 > 1000 || cat2 > 1000)
+        throw new RuntimeException("Algún cateto mayor que 1000");
+        return Math.hypot(cat1, cat2);
         }
-
-        return true; // Si no se encontró ningún divisor, es primo
-    }
-
-    public double calcularHipotenusa(double x, double y) {
-        
-        double hipotenusa = Math.sqrt(Math.pow(x, 2) + Math.pow(y, 2));
-        
-        return Math.round(hipotenusa * 100.0) / 100.0; // Redondeo a 2 decimales
-     
-    }
-
-    public List<Integer> calcularDivisores(int divNum) {
-        List<Integer> divisores = new ArrayList<>();
-        for (int i = 1; i <= divNum; i++) {
-            if (divNum % i == 0) {
-                divisores.add(i);
-            }
-        }
-        return divisores;
-    }
 }
